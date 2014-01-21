df491103664777ef106ab1c987294fa7ea61b50e
ozkriff <ozkriff@gmail.com>
2014-01-21 21:48:50 +0400

    misc.rs: Removed unused print_mat4

--- misc.rs
+++ misc.rs
@@ -1,23 +1,8 @@
 // See LICENSE file for copyright and license details.
 
 use std;
-use cgmath::matrix::{
-  Mat4,
-  Matrix
-};
 
-pub fn print_mat4(name: &str, mat: Mat4<f32>) {
-  println!("{}:", name);
-  for i in range(0u, 4) {
-    for j in range(0u, 4) {
-      print!("{} ", *mat.cr(i, j));
-    }
-    println!("");
-  }
-  println!("");
-}
-
-/// usage: let s = mvp_matrix; println(type_of(&s));
+/// usage: let f = 1.0f32; println(type_of(&f));
 pub fn type_of<T>(_: &T) -> &'static str {
   unsafe {
     (*std::unstable::intrinsics::get_tydesc::<T>()).name
