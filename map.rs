c698bd2ce82fc969dc4d73b9e423fb4fe8fbf1d8
ozkriff <ozkriff@gmail.com>
2014-01-28 08:14:31 +0400

    map.rs: Fixed TileIterator

--- map.rs
+++ map.rs
@@ -9,7 +9,7 @@ pub struct TileIterator {
 impl TileIterator {
   pub fn new() -> TileIterator {
     TileIterator {
-      cursor: Vec2::<i32>{x: 0, y: 0}
+      cursor: Vec2::zero()
     }
   }
 }
@@ -17,16 +17,17 @@ impl TileIterator {
 impl Iterator<Vec2<i32>> for TileIterator {
   fn next(&mut self) -> Option<Vec2<i32>> {
     let map_size = Vec2::<i32>{x: 3, y: 4};
-    self.cursor.x += 1;
-    if self.cursor.x == map_size.x {
-      self.cursor.x = 0;
-      self.cursor.y += 1;
-    }
-    if self.cursor.y == map_size.y {
+    let current_pos = if self.cursor.y > map_size.y {
       None
     } else {
       Some(self.cursor)
+    };
+    self.cursor.x += 1;
+    if self.cursor.x > map_size.x {
+      self.cursor.x = 0;
+      self.cursor.y += 1;
     }
+    current_pos
   }
 }
 
