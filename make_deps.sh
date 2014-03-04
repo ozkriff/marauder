4f513b5c750cd667b24d0494a752b81fee93b630
ozkriff <ozkriff@gmail.com>
2014-03-05 02:47:19 +0400

    Simplified make_deps.sh

--- make_deps.sh
+++ make_deps.sh
@@ -5,38 +5,28 @@ echo Creating 'deps' dir...
 mkdir deps
 cd deps
 
-# glfw
-echo Downloading glfw...
+echo === glfw3 ===
 git clone --depth=1 https://github.com/glfw/glfw.git
-echo Building glfw...
 cd glfw
 cmake -DBUILD_SHARED_LIBS=ON
 make glfw # build without examples or tests
 cp src/libglfw.so* .. # copy dynamic libraries to 'deps' dir
 cd ..
 
-# glfw-rs
-echo Downloading glfw-rs...
+echo === glfw-rs ===
 git clone --depth=1 https://github.com/bjz/glfw-rs
-echo Building glfw-rs...
 rustc glfw-rs/src/lib/lib.rs --out-dir .
 
-# gl-rs
-echo Downloading gl-rs...
+echo === gl-rs ===
 git clone --depth=1 https://github.com/bjz/gl-rs
-echo Building gl-rs...
 rustc gl-rs/src/gl/lib.rs --out-dir .
 
-# cgmath-rs
-echo Downloading cgmath-rs...
+echo === cgmath-rs ===
 git clone --depth=1 https://github.com/bjz/cgmath-rs
-echo Building cgmath-rs...
 rustc cgmath-rs/src/cgmath/lib.rs --out-dir .
 
-# rust-stb-image
-echo Downloading rust-stb-image...
+echo === rust-stb-image ===
 git clone --depth=1 https://github.com/mozilla-servo/rust-stb-image
-echo Building rust-stb-image...
 cd rust-stb-image
 ./configure
 make
