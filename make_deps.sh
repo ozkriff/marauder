858a2703a10c0baed1101567e9cc3a00b25bbdf6
ozkriff <ozkriff@gmail.com>
2014-03-27 13:06:24 +0400

    make_deps.sh: Added functions

--- make_deps.sh
+++ make_deps.sh
@@ -2,31 +2,60 @@
 #!/bin/sh
 set -e
 
-echo Creating 'deps' dir...
-mkdir deps
-cd deps # '.' -> './deps'
+get_glfw() {
+    echo === glfw ===
+    git clone --depth=1 https://github.com/glfw/glfw.git
+    cd glfw
+    cmake -DBUILD_SHARED_LIBS=ON
+    make glfw
+    mv src/lib*.so* ..
+    cd ..
+}
 
-echo === glfw-rs ===
-git clone --depth=1 https://github.com/bjz/glfw-rs
-cd glfw-rs
-git clone --depth=1 https://github.com/glfw/glfw.git
-cd glfw; cmake -DBUILD_SHARED_LIBS=ON; make glfw; mv src/lib*.so* ..; cd ..
-PKG_CONFIG_PATH=glfw/src make lib
-mv lib/*.rlib lib/*.so *.so* ..
-cd ..
+get_glfw_rs() {
+    echo === glfw-rs ===
+    git clone --depth=1 https://github.com/bjz/glfw-rs
+    cd glfw-rs
+    get_glfw
+    PKG_CONFIG_PATH=glfw/src make lib
+    mv lib/*.rlib lib/*.so *.so* ..
+    cd ..
+}
 
-echo === gl-rs ===
-git clone --depth=1 https://github.com/bjz/gl-rs
-rustc gl-rs/src/gl/lib.rs --out-dir .
+get_gl_rs() {
+    echo === gl-rs ===
+    git clone --depth=1 https://github.com/bjz/gl-rs
+    rustc gl-rs/src/gl/lib.rs --out-dir .
+}
 
-echo === cgmath-rs ===
-git clone --depth=1 https://github.com/bjz/cgmath-rs
-rustc cgmath-rs/src/cgmath/lib.rs --out-dir .
+get_cgmath_rs() {
+    echo === cgmath-rs ===
+    git clone --depth=1 https://github.com/bjz/cgmath-rs
+    rustc cgmath-rs/src/cgmath/lib.rs --out-dir .
+}
 
-echo === rust-stb-image ===
-git clone --depth=1 https://github.com/mozilla-servo/rust-stb-image
-cd rust-stb-image; ./configure; make; mv *.rlib *.a ..; cd ..
+get_stb_image() {
+    echo === rust-stb-image ===
+    git clone --depth=1 https://github.com/mozilla-servo/rust-stb-image
+    cd rust-stb-image
+    ./configure
+    make
+    mv *.rlib *.a ..
+    cd ..
+}
 
-cd .. # './deps' -> '.'
+get_deps() {
+    echo Creating 'deps' dir...
+    mkdir deps
+    cd deps
+    get_glfw_rs
+    get_gl_rs
+    get_cgmath_rs
+    get_stb_image
+    cd ..
+}
 
+get_deps
 echo Done!
+
+# vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
